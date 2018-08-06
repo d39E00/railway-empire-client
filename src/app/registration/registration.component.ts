f66641ddef7aee7f1502ece69c4458c0be0c9360
ElinaValieva <veaufa@mail.ru>
2018-08-06 21:19:44 +0300

    feat: register user

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {User} from '../models/user';
+import {UserService} from '../service/user.service';
 
 @Component({
-  selector: 'my-registration',
+  selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 
 export class RegistrationComponent {
-  model: any = {};
 
-  onSubmit(f: NgForm) {
-    alert(f.value.login+f.value.password+f.value.firstName);  // { first: '', last: '' }
+  constructor(private userService: UserService) {
   }
 
+  onSubmit(f: NgForm) {
+    const user = new User(f.value.firstName, f.value.lastName, f.value.login, f.value.password, null, null);
+    this.userService.registration(user);
+  }
 }
