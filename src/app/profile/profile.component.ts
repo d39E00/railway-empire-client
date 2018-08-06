3fd06deae52ddbb2bfd27eb715fcf46f3e641ae9
ElinaValieva <veaufa@mail.ru>
2018-08-06 23:00:53 +0300

    feat: profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../service/user.service';
+import {NgForm} from '@angular/forms';
+import {User} from '../models/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
   }
 
+  constructor(private userService: UserService) {
+    this.profile();
+  }
+
+  onSubmit(f: NgForm) {
+    const user = new User(f.value.firstName, f.value.lastName, f.value.login, f.value.password, f.value.birthDay, f.value.sex);
+    this.userService.edit(user);
+  }
+
+  profile() {
+    this.userService.profile();
+  }
 }
