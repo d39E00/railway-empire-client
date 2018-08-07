7e5d2a46755c80914597efbf7c7981daa0bc8dca
ElinaValieva <veaufa@mail.ru>
2018-08-07 16:37:30 +0300

    feat: audit pages with component

--- src/app/audit/audit.component.ts
+++ src/app/audit/audit.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuditComponent implements OnInit {
 
+  visibleTrain = false;
+  visibleStation = false;
+  visibleAudit = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  deletedTrains() {
+    this.visibleAudit = false;
+    this.visibleStation = false;
+    this.visibleTrain = true;
+  }
+
+  deletedStations() {
+    this.visibleAudit = false;
+    this.visibleTrain = false;
+    this.visibleStation = true;
+  }
+
+  audit() {
+    this.visibleAudit = true;
+    this.visibleTrain = false;
+    this.visibleStation = false;
+  }
 }
