3298aababe099d43ef1fb962908c7d77b07c76fe
ElinaValieva <veaufa@mail.ru>
2018-08-08 22:37:41 +0300

    feat: train-service requests

--- src/app/service/train.service.ts
+++ src/app/service/train.service.ts
@@ -1,48 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class TrainService {
 
-  trains = [];
-  constructor() { }
+  URL_ADD_TRAIN = 'http://localhost:8000/train/add';
+  URL_GET_DELETED_TRAIN = 'http://localhost:8000/train/deletedTrains';
+  URL_REESTABLISH_TRAIN = 'http://localhost:8000/train/reestablish/';
+  URL_EDIT_TRAIN = 'http://localhost:8000/train/update';
+  URL_DELETE_TRAIN = 'http://localhost:8000/train/delete/';
+  URL_GET_ALL = 'http://localhost:8000/train/allTrains';
+
+  constructor(private httpClient: HttpClient) {
+  }
 
   add(train) {
-    /*
-    '/train/add'
-    */
-    alert('add train');
+    return this.httpClient.post(this.URL_ADD_TRAIN, train);
   }
 
   getDeleted() {
-    //  "/train/deletedTrains";
-    let train = { trainName: 'first', cntCarriage: '12', cntSeats: '30' };
-    this.trains.push(train);
-    return this.trains;
+    return this.httpClient.get(this.URL_GET_DELETED_TRAIN);
   }
 
   reestablishTrain(train) {
-    //"/train/reestablish/" + train
-    alert(train);
+    return this.httpClient.get(this.URL_REESTABLISH_TRAIN + train);
   }
 
   getAll() {
-    // var urlSearching = "/train/allTrains";
-    let train1 = { trainName: 'first', cntCarriage: '12', cntSeats: '30' };
-    let train2 = { trainName: 'first', cntCarriage: '12', cntSeats: '30' };
-    let train3 = { trainName: 'first', cntCarriage: '12', cntSeats: '30' };
-    this.trains.push(train1);
-    this.trains.push(train2);
-    this.trains.push(train3);
-    return this.trains;
+    return this.httpClient.get(this.URL_GET_ALL);
   }
 
-  edit(train) { 
-    // "/train/update";
-    alert(JSON.stringify(train));
-
+  edit(train) {
+    return this.httpClient.put(this.URL_EDIT_TRAIN, train);
   }
 
-  delete(train) { 
-    // "/train/delete/" + name;
+  delete(train) {
+    return this.httpClient.delete(this.URL_DELETE_TRAIN + train);
   }
 }
