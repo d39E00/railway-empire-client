79d8167ad5de71b9d9762b2d6e9e0e669191d015
ElinaValieva <veaufa@mail.ru>
2018-08-08 23:21:20 +0300

    fix: login component subscribe

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {User} from '../models/user';
 import {UserService} from '../service/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,11 +10,20 @@ import {UserService} from '../service/user.service';
 })
 
 export class LoginComponent {
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
   }
 
   onSubmit(f: NgForm) {
     const user = {username: f.value.username, password: f.value.password};
-    this.userService.login(user);
+    this.userService.login(user).subscribe(
+      response => {
+        alert(JSON.stringify(response));
+        alert('Success');
+        console.log(response);
+        this.router.navigateByUrl('/audit');
+      },
+      err => {
+        alert(JSON.stringify(err));
+      });
   }
 }
