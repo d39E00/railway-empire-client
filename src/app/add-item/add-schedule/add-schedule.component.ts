0a272365b8030bf18986bf5923b2526d5a992176
ElinaValieva <veaufa@mail.ru>
2018-08-09 13:38:47 +0300

    fix: add schedule component (TODO - swal)

--- src/app/add-item/add-schedule/add-schedule.component.ts
+++ src/app/add-item/add-schedule/add-schedule.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {ScheduleService} from '../../service/schedule.service';
+import {Schedule} from '../../models/schedule';
+import {TrainService} from '../../service/train.service';
+import {StationService} from '../../service/station.service';
 
 @Component({
   selector: 'app-add-schedule',
@@ -9,22 +12,30 @@ import {ScheduleService} from '../../service/schedule.service';
 })
 export class AddScheduleComponent implements OnInit {
 
-  constructor(private scheduleService: ScheduleService) {
+  stationsList: any = [];
+  trainsList: any = [];
+
+  constructor(private scheduleService: ScheduleService, private trainService: TrainService, private stationService: StationService) {
+    this.stationService.getAllForAutoComplete().subscribe(res => {
+      this.stationsList = res;
+    });
+    this.trainService.getAllForAutoComplete().subscribe(res => {
+      this.trainsList = res;
+    });
   }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
-    const schedule = {
-      stationDepartureName: f.value.stationDeparture,
-      stationArrivalName: f.value.stationArrival,
-      dateDeparture: f.value.dateDeparture.replace('T', ' ') + ':00',
-      dateArrival: f.value.dateArrival !== '' ? f.value.dateArrival.replace('T', ' ') + ':00' : '',
-      trainName: f.value.trainSchedule
-    };
+    const schedule = new Schedule();
+    schedule.stationDepartureName = f.value.stationDeparture;
+    schedule.stationArrivalName = f.value.stationArrival;
+    schedule.dateDeparture = f.value.dateDeparture.replace('T', ' ') + ':00';
+    schedule.dateArrival = f.value.dateArrival !== '' ? f.value.dateArrival.replace('T', ' ') + ':00' : '';
+    schedule.trainName = f.value.trainSchedule;
     this.scheduleService.add(schedule).subscribe(
-      data => alert(JSON.stringify(data)),
+      data => alert('success'),
       error => alert(JSON.stringify(error)));
   }
 }
