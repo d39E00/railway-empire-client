ff2908386a50f18edb6a2a0c9ca559238ebe1c57
ElinaValieva <veaufa@mail.ru>
2018-08-09 14:27:09 +0300

    feat: auth service (with test user login and pass)

---
+++ src/app/service/authentication.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class AuthenticationService {
+
+  login = 'login@mail.ru';
+  password = 'pass';
+
+  constructor() {
+  }
+
+  authorize(user) {
+    this.login = user.username;
+    this.password = user.password;
+  }
+
+  getAuthorized() {
+    return {
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Basic ' + btoa(this.login + ':' + this.password),
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Requested-With': 'XMLHttpRequest'
+      }, responseType: 'text'
+    };
+  }
+
+  getHeader() {
+    return {
+      'Accept': 'application/json',
+      'Authorization': 'Basic ' + btoa(this.login + ':' + this.password),
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'X-Requested-With': 'XMLHttpRequest'
+    };
+  }
+
+  getHeaderPost() {
+    return {
+      'Accept': 'application/json',
+      'Authorization': 'Basic ' + btoa(this.login + ':' + this.password),
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest'
+    };
+  }
+}
+
