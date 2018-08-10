7e686c07c5f4caffedaa2712b6c550801e860d82
ElinaValieva <veaufa@mail.ru>
2018-08-10 21:18:47 +0300

    fix: trip template view

--- src/app/trip/trip.component.ts
+++ src/app/trip/trip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-trip',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  tickets: any = [];
 
   ngOnInit() {
   }
 
+  download(ticket) {
+  }
 }
