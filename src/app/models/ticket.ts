92d0a1c84ecd11f8cd6d3f4cb46044c8ca060647
ElinaValieva <veaufa@mail.ru>
2018-08-10 22:07:53 +0300

    feat: ticket-model

---
+++ src/app/models/ticket.ts
@@ -0,0 +1,36 @@
+export class Ticket {
+  id: number;
+  userFirstName: string;
+
+  userLastName: string;
+
+  userLogin: string;
+
+  userBirthDay: string;
+
+  userSex: string;
+
+  scheduleStationDepartureName: string;
+
+  scheduleStationArrivalName: string;
+
+  scheduleDateDeparture: string;
+
+  scheduleTrainName: string;
+
+  scheduleDateArrival: string;
+
+  scheduleStationDepartureLatitude: number;
+
+  scheduleStationDepartureLongitude: number;
+
+  scheduleStationArrivalLatitude: number;
+
+  scheduleStationArrivalLongitude: number;
+
+  seatCarriage: number;
+
+  seatSeat: number;
+
+  price: number;
+}
