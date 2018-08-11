8795dfbf59a40dec8f0be0c8cc28117ffca884f0
ElinaValieva <veaufa@mail.ru>
2018-08-11 22:51:30 +0300

    feat: footer info

---
+++ src/app/ticket/footer-seats/footer-seats.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-footer-seats',
+  templateUrl: './footer-seats.component.html',
+  styleUrls: ['./footer-seats.component.css']
+})
+export class FooterSeatsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
