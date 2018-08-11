46e72f63baeb768d7cc7a9c748d54921fa9b0a75
ElinaValieva <veaufa@mail.ru>
2018-08-11 17:00:27 +0300

    fix: form schedule by all parameters with using schedule-service

--- src/app/schedule/schedule-by-dates-stations-train/schedule-by-dates-stations-train.component.ts
+++ src/app/schedule/schedule-by-dates-stations-train/schedule-by-dates-stations-train.component.ts
@@ -1,5 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {StationService} from '../../service/station.service';
+import {TrainService} from '../../service/train.service';
+import {ScheduleService} from '../../service/schedule.service';
+import {Schedule} from '../../models/schedule';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-schedule-by-dates-stations-train',
@@ -8,15 +13,46 @@ import {NgForm} from '@angular/forms';
 })
 export class ScheduleByDatesStationsTrainComponent implements OnInit {
 
-  constructor() {
+  stationsList: any = [];
+  trainsList: any = [];
+  schedules: Schedule[] = [];
+  @Output() messageEvent = new EventEmitter<Schedule[]>();
+
+  constructor(private scheduleService: ScheduleService, private trainService: TrainService, private stationService: StationService) {
+    this.stationService.getAllForAutoComplete().subscribe(res => {
+      this.stationsList = res;
+    });
+    this.trainService.getAllForAutoComplete().subscribe(res => {
+      this.trainsList = res;
+    });
   }
+
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
-    alert(f.value.dateDeparture + ' '
-      + f.value.dateArrival + ' ' + f.value.stationDeparture + ' ' + f.value.stationArrival + ' ' + f.value.train);
+    const schedule = new Schedule();
+    schedule.stationDepartureName = f.value.stationDeparture;
+    schedule.stationArrivalName = f.value.stationArrival;
+    schedule.trainName = f.value.train;
+    schedule.dateDeparture = f.value.dateDeparture;
+    schedule.dateArrival = f.value.dateArrival;
+    this.scheduleService.getByAllParameters(schedule).subscribe(res => {
+      this.schedules = res;
+      this.messageEvent.emit(this.schedules);
+    }, error => {
+      console.log(error);
+      swal({
+        title: 'Oops..', text: error.error.message.toString().split('[MESSAGE]:')[1], type: 'error'
+      });
+    });
   }
 
+  getDelayBetweenTwoDates(dateDeparture, dateArrival) {
+  }
+
+  findTicket(schedule, i) {
+
+  }
 }
