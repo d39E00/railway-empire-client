19adb89aabb7a6c196412e44973cd462437cb5af
ElinaValieva <veaufa@mail.ru>
2018-08-11 22:54:31 +0300

    fix: REST-service with new request

--- src/app/service/ticket.service.ts
+++ src/app/service/ticket.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthenticationService} from './authentication.service';
 import {Ticket} from '../models/ticket';
+import {Seat} from '../models/seat';
 
 @Injectable()
 export class TicketService {
@@ -10,8 +11,15 @@ export class TicketService {
   }
 
   URL_GET_TICKETS = 'http://localhost:8000/userMap/show';
+  URL_GET_SEATS = 'http://localhost:8000/schedule/seat/';
+
 
   getTickets() {
     return this.http.get<Ticket[]>(this.URL_GET_TICKETS, {headers: this.authService.getHeader()});
   }
+
+  getSeats(id) {
+    return this.http.get<Seat>(this.URL_GET_SEATS + id, {headers: this.authService.getHeader()});
+  }
 }
+
