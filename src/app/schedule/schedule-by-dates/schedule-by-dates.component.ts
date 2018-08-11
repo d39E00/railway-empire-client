521dda755959070c976ba6cc541419d1ea40e3b8
ElinaValieva <veaufa@mail.ru>
2018-08-11 16:59:48 +0300

    fix: form schedule by dates with using schedule-service

--- src/app/schedule/schedule-by-dates/schedule-by-dates.component.ts
+++ src/app/schedule/schedule-by-dates/schedule-by-dates.component.ts
@@ -1,5 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {Schedule} from '../../models/schedule';
+import swal from 'sweetalert2';
+import {ScheduleService} from '../../service/schedule.service';
 
 
 @Component({
@@ -9,13 +12,28 @@ import {NgForm} from '@angular/forms';
 })
 export class ScheduleByDatesComponent implements OnInit {
 
-  constructor() {
+  schedules: Schedule[] = [];
+  @Output() messageEvent = new EventEmitter<Schedule[]>();
+
+  constructor(private scheduleService: ScheduleService) {
   }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
-    alert(f.value.dateDeparture + '' + f.value.dateArrival);
+    const schedule = new Schedule();
+    schedule.dateDeparture = f.value.dateDeparture;
+    schedule.dateArrival = f.value.dateArrival;
+    this.scheduleService.getByDates(schedule).subscribe(res => {
+      this.schedules = res;
+      this.messageEvent.emit(this.schedules);
+    }, error => {
+      console.log(error);
+      swal({
+        title: 'Oops..', text: error.error.message.toString().split('[MESSAGE]:')[1], type: 'error'
+      });
+    });
   }
 }
+
