ee35f03f2a99e57e46f3520f587416647962c108
ElinaValieva <veaufa@mail.ru>
2018-08-11 17:51:32 +0300

    feat: transfer schedule

---
+++ src/app/models/schedule.transfer.ts
@@ -0,0 +1,25 @@
+export class ScheduleTransfer {
+  idScheduleDeparture: number;
+
+  idScheduleArrival: number;
+
+  stationDepartureName: string;
+
+  stationIntermediateName: string;
+
+  stationArrivalName: string;
+
+  trainDepartureName: string;
+
+  trainArrivalName: string;
+
+  dateDeparture: string;
+
+  dateIntermediateDeparture: string;
+
+  dateIntermediateArrival: string;
+
+  dateArrival: string;
+
+  price: number;
+}
