d16a8e1425e22b576e85ed77abbdb614f6cc941d
ElinaValieva <veaufa@mail.ru>
2018-08-11 17:00:07 +0300

    fix: form schedule by dates and stations with using schedule-service

--- src/app/schedule/schedule-by-dates-stations/schedule-by-dates-stations.component.ts
+++ src/app/schedule/schedule-by-dates-stations/schedule-by-dates-stations.component.ts
@@ -1,5 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {StationService} from '../../service/station.service';
+import {TrainService} from '../../service/train.service';
+import {ScheduleService} from '../../service/schedule.service';
+import {Schedule} from '../../models/schedule';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-schedule-by-dates-stations',
@@ -8,13 +13,38 @@ import {NgForm} from '@angular/forms';
 })
 export class ScheduleByDatesStationsComponent implements OnInit {
 
-  constructor() {
+  stationsList: any = [];
+  trainsList: any = [];
+  schedules: Schedule[] = [];
+  @Output() messageEvent = new EventEmitter<Schedule[]>();
+
+
+  constructor(private scheduleService: ScheduleService, private trainService: TrainService, private stationService: StationService) {
+    this.stationService.getAllForAutoComplete().subscribe(res => {
+      this.stationsList = res;
+    });
+    this.trainService.getAllForAutoComplete().subscribe(res => {
+      this.trainsList = res;
+    });
   }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
-    alert(f.value.dateDeparture + ' ' + f.value.dateArrival + ' ' + f.value.stationDeparture + ' ' + f.value.stationArrival);
+    const schedule = new Schedule();
+    schedule.stationDepartureName = f.value.stationDeparture;
+    schedule.stationArrivalName = f.value.stationArrival;
+    schedule.dateDeparture = f.value.dateDeparture;
+    schedule.dateArrival = f.value.dateArrival;
+    this.scheduleService.getByStationsAndDates(schedule).subscribe(res => {
+      this.schedules = res;
+      this.messageEvent.emit(this.schedules);
+    }, error => {
+      console.log(error);
+      swal({
+        title: 'Oops..', text: error.error.message.toString().split('[MESSAGE]:')[1], type: 'error'
+      });
+    });
   }
 }
