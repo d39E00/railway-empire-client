7beec303700c0ba902efd07980d83c47e687d6a0
ElinaValieva <veaufa@mail.ru>
2018-08-11 17:51:09 +0300

    fix: calculating delay and redirect on ticket-booking

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Schedule} from '../models/schedule';
 import {ScheduleTransfer} from '../models/schedule.transfer';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-schedule',
@@ -13,7 +14,7 @@ export class ScheduleComponent implements OnInit {
   @Input() schedulesTransfer: ScheduleTransfer[];
 
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
@@ -28,11 +29,23 @@ export class ScheduleComponent implements OnInit {
   }
 
   getDelayBetweenTwoDates(dateDeparture, dateArrival) {
+    const hourDiff = new Date(dateArrival).getTime() - new Date(dateDeparture).getTime();
+    const minDiff = Math.ceil(hourDiff / 60 / 1000);
+    const hDiff = hourDiff / 3600 / 1000;
+    const humanReadable: any = {};
+    humanReadable.hours = Math.floor(hDiff);
+    humanReadable.minutes = minDiff - 60 * humanReadable.hours;
+    if (humanReadable.minutes >= 0 && humanReadable.minutes < 10) {
+      (humanReadable.minutes === 0) ? humanReadable.minutes = '00' : humanReadable.minutes = '0' + humanReadable.minutes;
+    }
+    return humanReadable.hours + ':' + humanReadable.minutes;
   }
 
   findTicket(schedule, i) {
+    this.router.navigateByUrl('/ticket');
   }
 
   findTicketTransfer(schedule) {
+    this.router.navigateByUrl('/ticket');
   }
 }
