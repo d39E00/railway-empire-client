527358ac1c7e8870fbc187577587fcc181f2cb62
ElinaValieva <veaufa@mail.ru>
2018-08-11 17:00:46 +0300

    fix: form schedule by dates and train with using schedule-service

--- src/app/schedule/schedule-by-dates-train/schedule-by-dates-train.component.ts
+++ src/app/schedule/schedule-by-dates-train/schedule-by-dates-train.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Schedule} from '../../models/schedule';
+import {ScheduleService} from '../../service/schedule.service';
+import swal from 'sweetalert2';
+import {TrainService} from '../../service/train.service';
 
 
 @Component({
@@ -9,13 +13,39 @@ import { NgForm } from '@angular/forms';
 })
 export class ScheduleByDatesTrainComponent implements OnInit {
 
-  constructor() { }
+  trainsList: any = [];
+  schedules: Schedule[] = [];
+  @Output() messageEvent = new EventEmitter<Schedule[]>();
+
+  constructor(private scheduleService: ScheduleService, private trainService: TrainService) {
+    trainService.getAllForAutoComplete().subscribe(res => {
+      this.trainsList = res;
+    });
+  }
 
   ngOnInit() {
   }
 
-
   onSubmit(f: NgForm) {
-    alert(f.value.dateDeparture + ' ' + f.value.dateArrival + ' ' + f.value.train);
+    const schedule = new Schedule();
+    schedule.trainName = f.value.trainName;
+    schedule.dateDeparture = f.value.dateDeparture;
+    schedule.dateArrival = f.value.dateArrival;
+    this.scheduleService.getByDates(schedule).subscribe(res => {
+      this.schedules = res;
+      this.messageEvent.emit(this.schedules);
+    }, error => {
+      console.log(error);
+      swal({
+        title: 'Oops..', text: error.error.message.toString().split('[MESSAGE]:')[1], type: 'error'
+      });
+    });
+  }
+
+  getDelayBetweenTwoDates(dateDeparture, dateArrival) {
+  }
+
+  findTicket(schedule, i) {
+
   }
 }
