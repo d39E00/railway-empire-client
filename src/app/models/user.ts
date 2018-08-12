bd6b1e18ef8015969389706628b88ff8addd8191
ElinaValieva <veaufa@mail.ru>
2018-08-12 22:44:37 +0300

    fix: user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,10 +1,8 @@
 export class User {
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public login: string,
-    public password: string,
-    public birthDay: string,
-    public sex: string) {
-  }
+  firstName: string;
+  lastName: string;
+  login: string;
+  password: string;
+  birthDay: string;
+  sex: string;
 }
