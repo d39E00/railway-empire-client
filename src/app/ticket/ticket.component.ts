a1e59bb63aaa9787399e28dc122f2a78b4f52ae9
ElinaValieva <veaufa@mail.ru>
2018-08-12 17:55:10 +0300

    fix: ticket-booking page

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,17 +1,37 @@
-import {Component, OnInit} from '@angular/core';
-import {TicketService} from '../service/ticket.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.css']
 })
-export class TicketComponent implements OnInit {
+export class TicketComponent implements OnInit, OnDestroy {
 
-  constructor(private ticketService: TicketService) {
+  constructor(private routes: ActivatedRoute) {
   }
+
+  private sub: any;
+  ID: number;
+  ID_NEXT: number;
+  ID_PREV: number;
+  transfer = false;
 
   ngOnInit(): void {
+    this.sub = this.routes.params.subscribe(params => {
+      const type = params['type'];
+      if (type === 'direct') {
+        this.ID = +params['id'];
+      } else {
+        this.ID = +params['id_Departure'];
+        this.ID_NEXT = +params['id_Arrival'];
+        this.ID_PREV = this.ID;
+        this.transfer = true;
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
