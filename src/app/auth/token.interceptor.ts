f250d1219a5948fb6f39b4396e64eb077ad261f8
ElinaValieva <veaufa@mail.ru>
2018-08-14 23:29:39 +0300

    feat: error handler

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -3,21 +3,37 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor, HttpXsrfTokenExtractor
+  HttpInterceptor, HttpErrorResponse, HttpResponse
 } from '@angular/common/http';
 
 import {Observable} from 'rxjs/Observable';
+import swal from 'sweetalert2';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private tokenExtractor: HttpXsrfTokenExtractor) {
+  constructor(private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const headerName = 'X-XSRF-TOKEN';
-    const token = this.tokenExtractor.getToken() as string;
-    req = req.clone({headers: req.headers.set(headerName, token)});
-    return next.handle(req);
+    return next.handle(req)
+      .do((ev) => {
+        if (ev instanceof HttpResponse) {
+          console.log('Response was successful: ', ev);
+        }
+      })
+      .catch((error: any) => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 403) {
+            swal({title: 'Opps...', text: 'You try to get security resource ...', type: 'error'});
+            this.router.navigateByUrl('/error');
+          } else {
+            this.router.navigateByUrl('/login');
+            swal({title: 'Opps...', text: 'Something went wrong ...Unauthorized!', type: 'error'});
+          }
+        }
+        return Observable.throwError(error);
+      }) as any;
   }
 }
