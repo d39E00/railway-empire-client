4e05757da3fe5b547665e97799475f08f5c3743d
ElinaValieva <veaufa@mail.ru>
2018-08-15 20:11:11 +0300

    fix: roles and logOut router

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthenticationService} from '../service/authentication.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -14,10 +15,15 @@ export class MenuComponent {
   ROLE_USER: boolean;
   ROLE_MANAGER: boolean;
 
-  constructor(private authService: AuthenticationService) {
+  constructor(private authService: AuthenticationService, private router: Router) {
     this.ROLE_USER = this.authService.getUserRole();
     this.ROLE_MANAGER = this.authService.getManagerRole();
     this.ROLE_ADMIN = this.authService.getAdminRole();
+  }
+
+  logOut() {
+    this.authService.logOut();
+    this.router.navigateByUrl('/login');
   }
 
 }
