3cf2f006a9ac864894b9015e5b23c42fd92eb4f4
ElinaValieva <veaufa@mail.ru>
2018-08-15 20:10:47 +0300

    fix: login page, add auth-service when user logIn

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import {NgForm} from '@angular/forms';
 import {UserService} from '../service/user.service';
 import {Router} from '@angular/router';
 import swal from 'sweetalert2';
+import {AuthenticationService} from '../service/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,16 @@ import swal from 'sweetalert2';
 })
 
 export class LoginComponent {
-  constructor(private userService: UserService, private router: Router) {
+  constructor(private userService: UserService,
+              private router: Router,
+              private authService: AuthenticationService) {
   }
 
   onSubmit(f: NgForm) {
     const user = {username: f.value.username, password: f.value.password};
     this.userService.login(user).subscribe(
       response => {
+        this.authService.authorize(user, response);
         console.log(response);
         this.router.navigateByUrl('/home');
       },
