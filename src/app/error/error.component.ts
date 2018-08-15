d05fe528d1460c527c05fd8c7760a553e88be85b
ElinaValieva <veaufa@mail.ru>
2018-08-15 20:09:37 +0300

    fix: error page, add btn to get back (login/home page)

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-error',
@@ -7,5 +8,17 @@ import {Component} from '@angular/core';
 })
 
 export class ErrorComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  check() {
+    if (localStorage.getItem('token') && localStorage.getItem('roles')) {
+      this.router.navigateByUrl('/home');
+    } else {
+      this.router.navigateByUrl('/login');
+    }
+  }
 }
 
