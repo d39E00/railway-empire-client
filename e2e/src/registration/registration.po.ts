d9500d629bcb60014c2a47d3ac456f75cd65b1df
ElinaValieva <veaufa@mail.ru>
2018-08-18 18:49:17 +0300

    test(with protractor.js): fix registration component in app

--- e2e/src/registration/registration.po.ts
+++ e2e/src/registration/registration.po.ts
@@ -7,14 +7,9 @@ export class RegistrationPage {
 
   fillCredenticals(credentials) {
     element(by.css('[name="firstName"]')).sendKeys(credentials.firstName);
-    browser.sleep(1000);
     element(by.css('[name="lastName"]')).sendKeys(credentials.lastName);
-    browser.sleep(1000);
     element(by.css('[name="login"]')).sendKeys(credentials.login);
-    browser.sleep(1000);
     element(by.css('[name="password"]')).sendKeys(credentials.password);
-    browser.sleep(1000);
     element(by.css('[name="registration"]')).click();
-    browser.sleep(2000);
   }
 }
