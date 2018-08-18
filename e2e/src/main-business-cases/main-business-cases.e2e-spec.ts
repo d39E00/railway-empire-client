10cd50c69e3f7689fe81f291ad640a4069cbeffb
ElinaValieva <veaufa@mail.ru>
2018-08-18 18:52:42 +0300

    test(protractor.js): add main business case for  app + configs
    1. Log as ADMIN
    2. Create new station (Voronezh)
    3. Create new schedule with new station(Voronezh-Moscow 22 Aug)
    4. Log as USER
    5. Find new schedule
    6. Book ticket for this schedule
    7. Look all tickets in trip tabs

---
+++ e2e/src/main-business-cases/main-business-cases.e2e-spec.ts
@@ -0,0 +1,61 @@
+import {LoginPage} from '../login/login.po';
+import {MenuPage} from '../menu/menu.po';
+import {AddStationPage} from '../add-station/add-station.po';
+import {AddSchedulePage} from '../add-schedule/add-schedule.po';
+import {SchedulePage} from '../schedule/schedule.po';
+import {browser, protractor} from 'protractor';
+
+describe('Main business case', () => {
+  let newStation: AddStationPage;
+  let newSchedule: AddSchedulePage;
+  let schedule: SchedulePage;
+  let login: LoginPage;
+  let menu: MenuPage;
+  const name = 'Voronezh';
+  const stationDeparture = 'Voronezh';
+  const stationArrival = 'Moscow';
+  const date = '22.08.2018' + protractor.Key.TAB + '10:00:00';
+  const trainName = 'T120';
+  const dateDeparture = '22.08.2018';
+
+  beforeEach(() => {
+    newStation = new AddStationPage();
+    newSchedule = new AddSchedulePage();
+    schedule = new SchedulePage();
+    menu = new MenuPage();
+    login = new LoginPage();
+    const origFn = browser.driver.controlFlow().execute;
+
+    browser.driver.controlFlow().execute = function() {
+      const args = arguments;
+
+      // queue 100ms wait
+      origFn.call(browser.driver.controlFlow(), function() {
+        return protractor.promise.delayed(50);
+      });
+
+      return origFn.apply(browser.driver.controlFlow(), args);
+    };
+  });
+
+  it('Login as ADMIN create Voronezh station', () => {
+    login.navigateTo();
+    login.logWithTruthParametersAsAdmin();
+    menu.getTabNewItem();
+    newStation.addStation(name);
+  });
+
+  it('Login as ADMIN create schedule with new station', () => {
+    newSchedule.addSchedule(stationDeparture, stationArrival, trainName, date);
+    login.logOut();
+  });
+
+  it('Login as USER find and book ticket', () => {
+    login.navigateTo();
+    login.logWithTruthParametersAsUser();
+    menu.getTabSchedule();
+    schedule.findSchedule(stationDeparture, stationArrival, dateDeparture);
+    menu.navigateTo();
+    menu.getItemTickets();
+  });
+});
